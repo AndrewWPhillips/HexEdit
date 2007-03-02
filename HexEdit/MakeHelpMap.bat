353b0c5e3d48ac2203971e2046b5794019710ab6
andrew <aphillips801@gmail.com>
2007-03-02 12:14:15 +0000

    Fix creation of help map file for Robohelp - different location for MFC and BCG .hm file - changes needed to handle spaces in file name - output to HexEditMap.h (not .hm)
    git-svn-id: svn://gil-w2k8/HexEdit_Repository/trunk@158 295070fd-a27b-da4f-a32e-77c6a882a0e3

--- HexEdit/MakeHelpMap.bat
+++ HexEdit/MakeHelpMap.bat
@@ -1,6 +1,21 @@
 echo Creating map files
-copy /y helpid.hm+resource.hm+hlp\HexEdit.hm+I:\Micros~1\vc98\mfc\include\afxhelp.hm+D:\bcg590\BCGControlBar\help\BCGControlBar.hm HexeditMap.tmp
+
+REM Get MFC help IDs
+rem copy /y I:\Micros~1\vc98\mfc\include\afxhelp.hm  afxhelp.tmp
+copy /y "C:\Program Files\Microsoft Visual Studio .NET\Vc7\atlmfc\include\afxhelp.hm"  afxhelp.tmp
+
+REM Get BCG help IDs
+rem copy /y D:\bcg590\BCGControlBar\help\BCGControlBar.hm  bcghelp.tmp
+copy /y "I:\Devel\BCG6_2\BCGControlBar\Help\BCGControlBar.hm"  .\bcghelp.tmp
+
+REM Copy all help IDs into one file
+copy /y helpid.hm+resource.hm+hlp\HexEdit.hm+afxhelp.tmp+bcghelp.tmp  HexeditMap.tmp
+
+rem del afxhelp.tmp
+rem del bcghelp.tmp
+
+REM Make sure they all start with #define which is what RoboHelp expects
 sed "s/^HID/#define HID/"  <HexeditMap.tmp >HexeditMap2.tmp
-del HexeditMap.tmp
-sed "s/^AFX_HID/#define AFX_HID/"  <HexeditMap2.tmp >HTMLHelp\HexeditMap.hm
-del HexeditMap2.tmp
+rem del HexeditMap.tmp
+sed "s/^AFX_HID/#define AFX_HID/"  <HexeditMap2.tmp >HTMLHelp\HexeditMap.h
+rem del HexeditMap2.tmp
