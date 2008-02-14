2c2fa9f9dac2196b89c7aaa65530a820f93f005c
andrew <aphillips801@gmail.com>
2008-02-14 13:33:50 +0000

    Remove CFD and HLP files from install.
    git-svn-id: svn://gil-w2k8/HexEdit_Repository/trunk@244 295070fd-a27b-da4f-a32e-77c6a882a0e3

--- HexEdit/Touchf.bat
+++ HexEdit/Touchf.bat
@@ -1,10 +1,8 @@
-w:\bin\touch -t 0802010320 release\hexedit.exe
-w:\bin\touch -t 0802010320 hexedit.tip
-w:\bin\touch -t 0802010320 relnotes.txt
-w:\bin\touch -t 0802010320 testdata\order.txt
-w:\bin\touch -t 0802010320 hexedit.cfd
-w:\bin\touch -t 0802010320 release\hexedit.hlp
-w:\bin\touch -t 0802010320 HTMLHelp\HexEdit.CHM
-w:\bin\touch -t 0802010320 HexEdit.xml
-w:\bin\touch -t 0802010320 testdata\readme.txt
+w:\bin\touch -t 0802140320 release\hexedit.exe
+w:\bin\touch -t 0802140320 HTMLHelp\HexEdit.CHM
+w:\bin\touch -t 0802140320 hexedit.tip
+w:\bin\touch -t 0802140320 relnotes.txt
+w:\bin\touch -t 0802140320 testdata\order.txt
+w:\bin\touch -t 0802140320 testdata\readme.txt
+w:\bin\touch -t 0802140320 HexEdit.xml
 
