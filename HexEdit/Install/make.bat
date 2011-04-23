98993b6e580d931f74514201416b4731c7e0653b
andrew <aphillips801@gmail.com>
2011-04-23 06:12:55 +0000

    Remove WIX installer
    git-svn-id: svn://gil-w2k8/HexEdit_Repository/trunk@1058 295070fd-a27b-da4f-a32e-77c6a882a0e3

--- HexEdit/Install/make.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\wix\candle" HexEdit.wxs
-"C:\Program Files\wix\light.exe" -out HexEd4_0.msi HexEdit.wixobj wixui\WixUI_HexEdit.wixlib -loc "C:\Program Files\wix\lib\WixUI_en-us.wxl"
-
