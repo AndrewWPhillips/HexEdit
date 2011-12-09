f23e4048772822df75facf5fb32438f8c0f9848c
andrew <aphillips801@gmail.com>
2011-12-09 22:42:54 +0000

    Update post-build batch file to work properly - fix path to commands (sed.exe and ReplaceVistaIcon.exe) - fix path to afxhelp.hm for VS2010 - fix path to output HexEditPro.exe (for icon replacement)
    git-svn-id: svn://gil-w2k8/HexEdit_Repository/trunk@1210 295070fd-a27b-da4f-a32e-77c6a882a0e3

--- HexEdit/MakehelpV.bat
+++ HexEdit/MakehelpV.bat
@@ -12,16 +12,17 @@ ECHO Command IDs
 echo // MAKEHELP.BAT generated Help Map file. >"HTMLHELP\CmdIdMap.h"
 echo // Commands (ID_* and IDM_*) >>"HTMLHELP\CmdIdMap.h"
 makehm ID_,HID_,0x10000 IDM_,HIDM_,0x10000 resource.h >"CmdIdMap.tmp"
-sed "s/^HID/#define HID/"  <"CmdIdMap.tmp" >>"HTMLHELP\CmdIdMap.h"
+"C:\Program Files\Utilities\sed"  "s/^HID/#define HID/"  <"CmdIdMap.tmp" >>"HTMLHELP\CmdIdMap.h"
 del "CmdIdMap.tmp"
 
 ECHO MFC Command IDs
 rem copy /y I:\Micros~1\vc98\mfc\include\afxhelp.hm  AfxIdMap.tmp
 rem copy /y "C:\Program Files\Microsoft Visual Studio .NET\Vc7\atlmfc\include\afxhelp.hm"  AfxIdMap.tmp
 rem copy /y "C:\Program Files\Microsoft Visual Studio 8\VC\atlmfc\include\afxhelp.hm"  AfxIdMap.tmp
-copy /y "C:\Program Files\Microsoft Visual Studio 9.0\VC\atlmfc\include\afxhelp.hm"  AfxIdMap.tmp
-sed "s/^HID/#define HID/"  <AfxIdMap.tmp >AfxId2.tmp
-sed "s/^AFX_HID/#define AFX_HID/"  <AfxId2.tmp   >"HTMLHELP\AfxIdMap.h"
+rem copy /y "C:\Program Files\Microsoft Visual Studio 9.0\VC\atlmfc\include\afxhelp.hm"  AfxIdMap.tmp
+copy /y "C:\Program Files\Microsoft Visual Studio 10.0\VC\atlmfc\include\afxhelp.hm"  AfxIdMap.tmp
+"C:\Program Files\Utilities\sed"  "s/^HID/#define HID/"  <AfxIdMap.tmp >AfxId2.tmp
+"C:\Program Files\Utilities\sed"  "s/^AFX_HID/#define AFX_HID/"  <AfxId2.tmp   >"HTMLHELP\AfxIdMap.h"
 del AfxIdMap.tmp
 del AfxId2.tmp
 
@@ -29,7 +30,7 @@ ECHO Dialog IDs
 echo // MAKEHELP.BAT generated Help Map file. >"HTMLHELP\DlgIdMap.h"
 echo // Dialogs (IDD_*) >>"HTMLHELP\DlgIdMap.h"
 makehm IDD_,HIDD_,0x20000 resource.h >"DlgIdMap.tmp"
-sed "s/^HID/#define HID/"  <"DlgIdMap.tmp" >>"HTMLHELP\DlgIdMap.h"
+"C:\Program Files\Utilities\sed"  "s/^HID/#define HID/"  <"DlgIdMap.tmp" >>"HTMLHELP\DlgIdMap.h"
 del "DlgIdMap.tmp"
 
 ECHO Control IDs (for What's This help)
@@ -37,8 +38,9 @@ copy /y helpid.hm+resource.hm  HTMLHelp\CtlIdMap.h
 
 rem ECHO BCG Control IDs
 rem copy /y "D:\Devel\BCG6_2\BCGControlBar\Help\BCGControlBar.hm" BcgIdMap.tmp
-rem sed "s/^HID/#define HID/"  <BcgIdMap.tmp >"HTMLHELP\BcgIdMap.h"
+rem "C:\Program Files\Utilities\sed"  "s/^HID/#define HID/"  <BcgIdMap.tmp >"HTMLHELP\BcgIdMap.h"
 rem del BcgIdMap.tmp
 
 ECHO Fix up hexedit.exe icon since the resource compiler cannot handle the new icon format
-ReplaceVistaIcon  D:\Andrew\cpp_out\HexEdit\ReleaseVS2005\hexedit.exe   res\hexedit2.ico
+rem ReplaceVistaIcon  D:\Andrew\cpp_out\HexEdit\ReleaseVS2005\hexedit.exe   res\hexedit2.ico
+"C:\Program Files\Utilities\ReplaceVistaIcon"  "C:\andrew\cpp_out\HexEdit\ReleaseVS2010\HexEditPro.exe"   res\hexedit2.ico
