f608d827ff259f48589b192bdcb99e6a73c65100
andrew <aphillips801@gmail.com>
2011-12-09 22:34:48 +0000

    Fix display of background in About box.
    git-svn-id: svn://gil-w2k8/HexEdit_Repository/trunk@1207 295070fd-a27b-da4f-a32e-77c6a882a0e3

--- HexEdit/Register.cpp
+++ HexEdit/Register.cpp
@@ -154,7 +154,9 @@ BOOL CAbout::OnInitDialog()
 #ifdef USE_FREE_IMAGE
 BOOL CAbout::OnEraseBkgnd(CDC* pDC)
 {
-	FIBITMAP *dib = FreeImage_Load(FIF_BMP, ::GetExePath() + FILENAME_ABOUTBG);
+	FIBITMAP *dib = FreeImage_Load(FIF_JPEG, ::GetExePath() + FILENAME_ABOUTBG);
+	if (dib == NULL)
+		dib = FreeImage_Load(FIF_BMP, ::GetExePath() + FILENAME_ABOUTBG);
 	if (dib == NULL)
 		return CDialog::OnEraseBkgnd(pDC);
 
