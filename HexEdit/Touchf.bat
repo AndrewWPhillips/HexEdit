8123a93ba54189c525b5ecd7c9768ccfa90f2255
andrew <aphillips801@gmail.com>
2011-12-12 09:15:08 +0000

    Minor fixes for release 4.0
    git-svn-id: svn://gil-w2k8/HexEdit_Repository/trunk@1216 295070fd-a27b-da4f-a32e-77c6a882a0e3

--- HexEdit/Touchf.bat
+++ HexEdit/Touchf.bat
@@ -1,8 +1,12 @@
-touch 04290340 release\hexedit.exe
-touch 04290340 HTMLHelp\HexEdit.CHM
-touch 04290340 hexedit.tip
-touch 04290340 install\relnotes.txt
-touch 04290340 install\order.txt
-touch 04290340 install\readme.txt
-touch 04290340 HexEdit.xml
+rem touch 12120400 release\HexEditPro.exe
+rem touch 12120400 HTMLHelp\HexEditPro.CHM
+rem touch 12120400 HexEditPro.tip
+REM We needs a new version of touch that can handle long file names
+touch 12120400 release\HexEditP.exe
+touch 12120400 HTMLHelp\HexEditP.CHM
+touch 12120400 HexEditP.tip
+touch 12120400 install\relnotes.txt
+touch 12120400 install\order.txt
+touch 12120400 install\readme.txt
+touch 12120400 HexEdit.xml
 pause
