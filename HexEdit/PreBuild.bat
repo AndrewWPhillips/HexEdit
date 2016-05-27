4002aebb90863adc0ccf831796a86555f4e7cadf
Andrew Phillips <aphillips801@gmail.com>
2016-05-27 12:32:26 +1000

    Make prebuild batch file more adaptable by trying to find Git.exe

--- HexEdit/PreBuild.bat
+++ HexEdit/PreBuild.bat
@@ -1,6 +1,21 @@
 ECHO Get revision number from git
-echo // Pre build automatically generated header file> GitRevision.h
+echo // Automatically generated header file created by Prebuild.bat > GitRevision.h
+
 REM We use SET/P to do an echo without newline
-set /p="#define GITREVISION "                        >> GitRevision.h
+set /p="#define GITREVISION "                                       >> GitRevision.h
+
 REM Count number of commits (relies on Git being installed)
-git rev-list HEAD | find /v /c ""                    >> GitRevision.h
+IF NOT EXIST "C:\Program Files (x86)\Git\bin\git.exe" goto next1
+  "C:\Program Files (x86)\Git\bin\git.exe" rev-list HEAD | find /v /c "" >> GitRevision.h
+  goto end
+:next1
+
+IF NOT EXIST "C:\Program Files (x86)\SmartGit\git\bin\git.exe" goto next2
+  "C:\Program Files (x86)\SmartGit\git\bin\git.exe" rev-list HEAD | find /v /c "" >> GitRevision.h
+  goto end
+:next2
+
+REM Just try running git and hope it's in the path
+  git rev-list HEAD | find /v /c ""                                 >> GitRevision.h
+
+:end
