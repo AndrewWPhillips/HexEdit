8688a425d103d3b53f9108b0a4b9964902832dbe
Andrew Phillips <aphillips801@gmail.com>
2016-06-05 23:27:33 +1000

    Remove sed.exe from the repo as it already comes with Git.

--- HexEdit/MakehelpV.bat
+++ HexEdit/MakehelpV.bat
@@ -8,13 +8,13 @@ ECHO Command IDs
 echo // MAKEHELP.BAT generated Help Map file.  >"HTMLHELP\CmdIdMap.h"
 echo // Commands (ID_* and IDM_*)  >>"HTMLHELP\CmdIdMap.h"
 makehm  ID_,HID_,0x10000 IDM_,HIDM_,0x10000 resource.h  >"CmdIdMap.tmp"
-bin\sed  "s/^HID/#define HID/"  <"CmdIdMap.tmp"  >>"HTMLHELP\CmdIdMap.h"
+sed  "s/^HID/#define HID/"  <"CmdIdMap.tmp"  >>"HTMLHELP\CmdIdMap.h"
 del "CmdIdMap.tmp"
 
 ECHO MFC Command IDs
 copy /y HTMLHelp\afxhelp.hm  AfxIdMap.tmp
-bin\sed  "s/^HID/#define HID/"  <AfxIdMap.tmp  >AfxId2.tmp
-bin\sed  "s/^AFX_HID/#define AFX_HID/"  <AfxId2.tmp  >"HTMLHELP\AfxIdMap.h"
+sed  "s/^HID/#define HID/"  <AfxIdMap.tmp  >AfxId2.tmp
+sed  "s/^AFX_HID/#define AFX_HID/"  <AfxId2.tmp  >"HTMLHELP\AfxIdMap.h"
 del AfxIdMap.tmp
 del AfxId2.tmp
 
@@ -22,7 +22,7 @@ ECHO Dialog IDs
 echo // MAKEHELP.BAT generated Help Map file.  >"HTMLHELP\DlgIdMap.h"
 echo // Dialogs (IDD_*)  >>"HTMLHELP\DlgIdMap.h"
 makehm  IDD_,HIDD_,0x20000 resource.h  >"DlgIdMap.tmp"
-bin\sed  "s/^HID/#define HID/"  <"DlgIdMap.tmp"  >>"HTMLHELP\DlgIdMap.h"
+sed  "s/^HID/#define HID/"  <"DlgIdMap.tmp"  >>"HTMLHELP\DlgIdMap.h"
 del "DlgIdMap.tmp"
 
 ECHO Control IDs (for What's This help)
